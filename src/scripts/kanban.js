0773631e20c03da9a1706292512151ce48a782da
Jerric1801 <Jerric1801@gmail.com>
2024-03-02 15:24:33 +0800

    added kanban functionality

--- src/scripts/kanban.js
+++ src/scripts/kanban.js
@@ -1,93 +1,97 @@
 // use jquery for this class
+function calculateIntersectionArea(rect1, rect2) {
+    const xOverlap = Math.max(0, Math.min(rect1.left + rect1.width, rect2.left + rect2.width) - Math.max(rect1.left, rect2.left));
+    const yOverlap = Math.max(0, Math.min(rect1.top + rect1.height, rect2.top + rect2.height) - Math.max(rect1.top, rect2.top));
+    const overlapArea = xOverlap * yOverlap;
+    return overlapArea;
+}
 
 class kanban {
+
+    position = null
+
     constructor() {
         this.draggables = $(".draggable")
-
+        this.board = $("#work-kanban")
         this.init_drop()
         this.init_drag()
     }
     init_drop() {
-        $("#work-kanban").droppable({
-
-        })
+        this.board.droppable()
     }
     init_drag() {
         this.draggables.each((index, drag_item)=>{
-            $(drag_item).draggable({
-                zIndex: 9999,
-                revert: "valid",
-                containment: "#work-kanban",
-                connectWith: "#work-kanban" // Link with the sortable
-            })
+            this.apply_drag(drag_item);
         })
+    }
+    apply_drag(drag_item) {
+        $(drag_item).draggable({
+            cursor: "move",
+            revert: "valid",
+            containment: "#work-kanban",
+            start: (event, ui) =>  {
+            },
+            drag: (event, ui)=> {
+                $(drag_item).css("z-index", 9999)
+                this.position = $(drag_item).offset()
+            },
+            stop : (event, ui) => {
+                $(drag_item).css("z-index", 1)
+                const mostIntersected = this.calculate_intersection($(drag_item))
+                const dragIndex = this.board.index($(drag_item))
+                const intersectIndex = this.board.index(mostIntersected)
+                console.log(dragIndex)
+                if (mostIntersected && this.valid_drop(dragIndex, intersectIndex)) {
+                    $(drag_item).insertBefore(mostIntersected); // Insert directly after
+                } else {
+                    this.board.append($(drag_item)); // Fallback to append if no intersection
+                }
+            }
+        }
+        )
+    }
+    calculate_intersection(drag_item) {
+        let mostIntersected = null;
+        let maxIntersectionArea = 0;
+
+        const position = this.position
+
+        const draggedRect = { // Get dimensions of the dragged item
+            top: position.top,
+            left: position.left,
+            width: $(drag_item).width(),
+            height: $(drag_item).height()
+        };
+
+        this.draggables.each((index, otherDragItem) => {
+            if ($(drag_item).attr('class') !== $(otherDragItem).attr('class')) { 
+                const otherPosition = $(otherDragItem).offset(); 
+                const otherRect = {
+                    top: otherPosition.top,
+                    left: otherPosition.left,
+                    width: $(otherDragItem).width(),
+                    height: $(otherDragItem).height()
+                };
+
+                const intersectionArea = calculateIntersectionArea(draggedRect, otherRect);
+
+                if (intersectionArea > maxIntersectionArea) {
+                    maxIntersectionArea = intersectionArea;
+                    mostIntersected = otherDragItem;
+                }
+            }
+        })
+        return mostIntersected;
+    }
+    valid_drop(dragIndex, intersectIndex){
+        const big_widgets = ["map-widget", "stock-widget"]
+        //SPLIT BY ROWS, IF THE RESULTING POSITIONS HAVE 2 BIG WIDGETS IN ONE HALF - THAT ARE NOT 01 OR 56, DISALLOW
+        // if (big_widgets.includes())
+        console.log(dragIndex)
+        return true
     }
 }
 
 $(window).on("load", function() {
     new kanban()
 })
-
-
-// init_drop() {
-//     const kanban_top = $("#work-kanban").offset().top
-//     const height = 250 * 2 + 50;
-//     this.droppables.sortable({
-//         sort: function(event, ui) {
-//             const rowHeight = 250; 
-//             const maxRows = 2; 
-//             const currentHeight = $("#work-kanban").height()
-        
-//             const placeholderRow = Math.floor((currentHeight) / (rowHeight));
-
-//             console.log(placeholderRow)
-//             if (placeholderRow > maxRows) {
-//                 const row_one = []
-//                 const row_two = []
-//                 const elements_to_restore = [];
-
-//                 $("#work-kanban").children().each(function(){
-//                     let element_top = $(this).offset().top
-//                     let difference = element_top - kanban_top;
-//                     if (difference > -height) {
-//                         elements_to_restore.push($(this))
-//                     }
-//                     else if (difference > -height/2 && difference < -height) {
-//                         row_two.push($(this))
-//                     }
-//                     else {
-//                         row_one.push($(this))
-//                     }
-                    
-//                 })
-//                 $(elements_to_restore).each(function() {
-//                     let element_width = $(this).width;
-//                     //check first row
-//                     let sum_width = 0;
-//                     $(row_one).each(function() {
-//                         sum_width += element_width
-//                     })
-
-//                     let actual_width = $("#work-kanban").width() - 250;
-//                     if (sum_width < actual_width){
-//                         $(this).insertAfter(`#work-kanban:nth-child(${row_one.length})`)
-//                     }
-
-//                     // sum_width = 0;
-//                     // $(row_two).each(function() {
-//                     //     sum_width += element_width
-//                     // })
-
-//                     // if (sum_width < actual_width){
-//                     //     $(this).insertAfter(`#work-kanban:nth-child(${row_two.length})`)
-//                     // }
-
-
-//                 })
-
-//             } 
-//         }
-//     }
-//     );
-//     this.droppables.disableSelection();
-// }
