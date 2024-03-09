09baffc08c6555ac4414dbd08a095d2ecd9e2473
Jerric1801 <Jerric1801@gmail.com>
2024-03-09 21:53:17 +0800

    added mapping buddy content

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,17 +1,13 @@
 class Project {
     name;
     image_path = "./static/images/";
-    images;
-    desc;
     popup = document.getElementById("works-popup")
     content = document.getElementById("popup-content");
     backButton = document.getElementById("popup-back");
 
-    constructor(name, path, images, desc) {
+    constructor(name, path, sections) {
         this.name = name;
         this.image_path += path;
-        this.images = images;
-        this.desc = desc;
 
         //clear popup
         this.content.innerHTML = ""
@@ -21,8 +17,31 @@ class Project {
 
         this.bind_back_button()
         this.disableScrolling()
-        this.create_title()
-        this.create_layout()
+        // this.create_title()
+        for (const key in sections) {
+            const value = sections[key]
+            this.section_router(value, key)
+        }
+    }
+    section_router(value, key) {
+        if (key == "intro"){
+            this.create_intro(value)
+        }
+        else if (key == "layout_1") {
+            this.create_default(value, "popup-default-1")
+        }
+        else if (key == "layout_2") {
+            this.create_default(value, "popup-default-2")
+        }
+        else if (key == "layout_3") {
+            this.create_default(value, "popup-default-3")
+        }
+        else if (key == "gallery_1") {
+            this.create_gallery(value, "popup-gallery-3")
+        }
+        else if (key == "gallery_2") {
+            this.create_gallery(value, "popup-gallery-5")
+        }
     }
     bind_back_button() {
         this.backButton.addEventListener("click", () => {
@@ -55,49 +74,71 @@ class Project {
 
         this.content.append(titleCon)
     }
-    create_image(src, type= 0) {
+    create_image(src) {
         let image = document.createElement("img")
         image.src = this.image_path + "/" + src
 
         return image
     }
-    create_description(content, type=0) {
+    create_description(content, className = "") {
         let descCon = document.createElement("div")
+        descCon.className = className + " popup-description"
         let text = document.createElement("p")
         text.textContent = content
         descCon.append(text)
 
         return descCon
     }
-    create_layout() {
-        const description = this.desc
-        const images = this.images
-        images.map((image, i) => {
-            var desc = description[i]
-            let projectCon = document.createElement("div")
-            projectCon.className = "popup-project-container"
-            let imageItem = this.create_image(image)
-            let descItem = this.create_description(desc)
+    create_intro(value) {
+        const [logo, old_description] = value
+        const description = old_description.split(" * ")
+        let introCon = document.createElement("div")
+        let imageItem = this.create_image(logo)
+        let projectType = this.create_description(description[0], "center bold")
+        let skillsUsed = this.create_description(description[1], "center bold")
+        let descItem = this.create_description(description[2],  "center")
+        introCon.className = "popup-project-intro popup-default"
+        
+        introCon.append(imageItem)
+        introCon.append(projectType)
+        introCon.append(skillsUsed)
+        introCon.append(descItem)
 
-            projectCon.append(imageItem)
-            projectCon.append(descItem)
+        this.content.append(introCon)
+    }
+    create_default(value, className) {
+        const [image, description] = value
+        let projectCon = document.createElement("div")
+        projectCon.className = "popup-default" + " " + className
+        let imageItem = this.create_image(image)
+        let descItem = this.create_description(description)
 
-            this.content.append(projectCon)
-        })
+        projectCon.append(imageItem)
+        projectCon.append(descItem)
+
+        this.content.append(projectCon)
     }
 }
 
 const project_mapper = 
 {"mapping_buddy": {
     "name": "Mapping Buddy",
-    "image": ["overview.png", "CU.png", "showcase.png"],
-    "desc": ["this is the overview", "hello", "hello"]
+    "sections": {
+        "intro": ["mapping_buddy.png", "SMU Module IS201 Interaction Design Prototyping * HTML, CSS, Javascript, Jquery UI * Frustrated with unintuitive tools and scattered information, SMU students struggle to effectively plan their modules. My team designed Mapping Buddy, a web application that simplifies course sequence planning. It offers a streamlined user experience, potentially saving students countless hours of hassle" ],
+        "layout_1": ["overview.png", "" ]
+    }
 },
 "birthin": {
     "name": "birthin co.",
+    "sections": {
+        "intro": ["birthin.png", "SMU Module IS201 Interaction Design Prototyping * HTML, CSS, Javascript * Mapping Buddy is a project that was done in school" ],
+    }
 },
 "mood": {
     "name": "mood",
+    "sections": {
+        "intro": ["mood_logo.png", "SMU Module IS113 Sensors and IOT<br>HTML, CSS, Javascript * Mapping Buddy is a project that was done in school" ],
+    }
 },
 "gis": {
     "name": "Derna Deluge Dorsier",
@@ -120,7 +161,7 @@ const project_mapper =
 export function build_project(attr) {
     try{
         let project_info = project_mapper[attr]
-        new Project(project_info["name"], attr, project_info["image"], project_info["desc"])
+        new Project(project_info["name"], attr, project_info["sections"])
 
     }
     catch {
